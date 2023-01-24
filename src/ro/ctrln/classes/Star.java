a495d07167921daf3f24457588fb8b5d15591cef
Andreea <andreeamaria1510@yahoo.com>
2023-01-25 00:47:49 +0200

    Vectors

--- src/ro/ctrln/classes/Star.java
+++ src/ro/ctrln/classes/Star.java
@@ -5,11 +5,14 @@ import java.math.BigDecimal;
 public class Star {
 
 
-    private String starDescription;
+    private String starDescription; //variabila de instanta toate 5
     private int diameter;
     private int satellites;
     private BigDecimal mass;
     private SmallPlanet smallPlanet;
+
+    public static final boolean MYLKYWAYSTAR = true; //variabila de clasa
+
 
     public Star() {
     }
@@ -52,8 +55,25 @@ public class Star {
     public void setMass(BigDecimal mass) {
         this.mass = mass;
     }
+
+    public void setSmallPlanet(SmallPlanet smallPlanet /*parametrul metodei*/) {
+        this.smallPlanet /*variabila de instanta*/ = smallPlanet; //parametrul metodei
+    }
+
+    public SmallPlanet getSmallPlanet(){
+        return this.smallPlanet; //variabila de instanta
+
+    }
+    public String computeSmallPlanetLocation(SmallPlanet smallPlanet, int location) {
+        String planetLocation = this.starDescription + smallPlanet.getPlanetName() + location; //variabila locala
+        return "";
+    }
 }
 
     class SmallPlanet {
     private String planetName;
-}
+
+        public String getPlanetName() {
+            return this.planetName;
+        }
+    }
